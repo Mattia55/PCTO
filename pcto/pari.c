477bf220ad90e9bb83565a00099478b32f66cac7
Mattia55 <127745791+Mattia55@users.noreply.github.com>
2023-03-14 16:48:06 +0100

    Add files via upload

---
+++ pcto/pari.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 100;
+    while (i>=0) {
+        if (i%2 == 0) {
+            printf("%d è pari\n", i);
+        } else {
+            //
+        }
+        i--;
+    }
+}
